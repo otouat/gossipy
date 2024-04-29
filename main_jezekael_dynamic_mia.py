b16a6cb0339d4d64eb39c17fba5cfc1345fbb518
Jezbrunon <jezekael.brunon@gmail.com>
2024-04-29 04:10:45 +0200

    Auto commit and push for testing

--- main_jezekael_dynamic_mia.py
+++ main_jezekael_dynamic_mia.py
@@ -17,7 +17,7 @@ from gossipy.mia.utils import log_results
 
 # Dataset loading
 transform = Compose([Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
-train_set, test_set = get_CIFAR10()
+train_set, test_set = get_CIFAR100()
 nodes_num = 100
 num_classes = max(train_set[1].max().item(), test_set[1].max().item())+1
 
