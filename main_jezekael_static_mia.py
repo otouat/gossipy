88f17cf2aa7a375a837eef085c7413860a6827ac
Jezbrunon <jezekael.brunon@gmail.com>
2024-05-02 09:59:06 +0200

    V6.3

--- main_jezekael_static_mia.py
+++ main_jezekael_static_mia.py
@@ -1,23 +1,34 @@
 import torch
 import torch.nn.functional as F
 from torchvision.transforms import Compose, Normalize
-from gossipy.core import AntiEntropyProtocol, CreateModelMode, StaticP2PNetwork
+from gossipy.core import AntiEntropyProtocol, CreateModelMode, ConstantDelay, StaticP2PNetwork
 from gossipy.data import CustomDataDispatcher
 from gossipy.data.handler import ClassificationDataHandler
 from gossipy.model.handler import TorchModelHandler
-from gossipy.node import GossipNode
-from gossipy.simul import MIAGossipSimulator, MIASimulationReport
-from gossipy.model.architecture import resnet20, resnet9
+from gossipy.node import GossipNode, FederatedGossipNode
+from gossipy.simul import MIAGossipSimulator, MIADynamicGossipSimulator, MIAFederatedSimulator, MIASimulationReport
+from gossipy.model.architecture import *
 from gossipy.model.resnet import *
 from gossipy.data import get_CIFAR10, get_CIFAR100
-from gossipy.topology import create_torus_topology, CustomP2PNetwork
+from gossipy.topology import create_torus_topology, create_federated_topology, CustomP2PNetwork
 from gossipy.mia.utils import log_results
 
 transform = Compose([Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
-train_set, test_set = get_CIFAR10()
-n_nodes = 36
-num_classes= max(train_set[1].max().item(), test_set[1].max().item())+1
+train_set, test_set = get_CIFAR100()
 
+
+n_classes= max(train_set[1].max().item(), test_set[1].max().item())+1
+model = resnet20(n_classes)
+n_nodes = 100
+n_rounds = 250
+n_local_epochs = 5
+batch_size= 256
+optimizer_params = {
+        "lr": 0.1,
+        "momentum": 0.9,
+        "weight_decay": 0.001
+    }
+message = "Experiment with ResNet20 on CIFAR100 dataset. 100 nodes, 250 rounds, 5 local epochs, batch size 128, lr 0.1."
 
 Xtr, ytr = transform(train_set[0]), train_set[1]
 Xte, yte = transform(test_set[0]), test_set[1]
@@ -25,7 +36,7 @@ Xte, yte = transform(test_set[0]), test_set[1]
 
 data_handler = ClassificationDataHandler(Xtr, ytr, Xte, yte, test_size=0.5)
 
-data_dispatcher = CustomDataDispatcher(data_handler, n=n_nodes*2, eval_on_user=True, auto_assign=True)
+data_dispatcher = CustomDataDispatcher(data_handler, n=n_nodes, eval_on_user=True, auto_assign=True)
 
 topology = create_torus_topology(n_nodes)
 network = CustomP2PNetwork(topology)
@@ -34,17 +45,13 @@ nodes = GossipNode.generate(
     data_dispatcher=data_dispatcher,
     p2p_net=network,
     model_proto=TorchModelHandler(
-        net=resnet20(num_classes),
-        optimizer= torch.optim.Adam,
-        optimizer_params = {
-            "lr": 0.1,
-            "momentum": 0.9,
-            "weight_decay": 0.001
-        },
+        net=model,
+        optimizer=torch.optim.SGD,
+        optimizer_params = optimizer_params,
         criterion = F.cross_entropy,
         create_model_mode= CreateModelMode.MERGE_UPDATE,
-        batch_size= 256,
-        local_epochs= 5),
+        batch_size= batch_size,
+        local_epochs= n_local_epochs),
     round_len=100,
     sync=False)
 
@@ -53,12 +60,14 @@ simulator = MIAGossipSimulator(
     data_dispatcher=data_dispatcher,
     delta=100,
     protocol=AntiEntropyProtocol.PUSH,
+    online_prob=1,  # Approximates the average online rate of the STUNner's smartphone traces
+    drop_prob=0,  # 0.1 Simulate the possibility of message dropping,
     sampling_eval=0,
 )
 
 report = MIASimulationReport()
 simulator.add_receiver(report)
 simulator.init_nodes(seed=42)
-simulator.start(n_rounds=100)
+simulator.start(n_rounds=n_rounds)
 
-log_results(simulator, report, topology)
+log_results(simulator, report, topology, message)
