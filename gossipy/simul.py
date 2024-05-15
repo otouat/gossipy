830ae17be654ee7adb68bf7c04ba26855dd7af5b
Jezbrunon <jezekael.brunon@gmail.com>
2024-05-15 11:37:26 +0200

    V9.12

--- gossipy/simul.py
+++ gossipy/simul.py
@@ -1310,7 +1310,7 @@ class MIAGossipSimulator(GossipSimulator):
                             er.update_mia_vulnerability(self.n_rounds, mia_vulnerability)
                             mia_mar_vulnerability = [mia_for_each_nn(self, n, class_specific=False, marginalized=True) for _, n in self.nodes.items() if isinstance(n, AttackGossipNode) and getattr(n, 'marginalized_state', False)]
                             ra_mar_vulnerability = [ra_for_each_nn(self, n, marginalized=True) for _, n in self.nodes.items() if isinstance(n, AttackGossipNode) and getattr(n, 'marginalized_state', False)]
-                            print(ra_mar_vulnerability)
+                            #print(ra_mar_vulnerability)
                             if any(item is not None for item in mia_mar_vulnerability):
                                 er.update_mia_vulnerability(self.n_rounds, mia_mar_vulnerability, marginalized = True)
                                 print(ra_mar_vulnerability)
