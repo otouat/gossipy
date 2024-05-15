50121a62fb4599818349e39b0f84e1fac3a6a075
Jezbrunon <jezekael.brunon@gmail.com>
2024-05-15 16:46:28 +0200

    test

--- gossipy/ra/ra.py
+++ gossipy/ra/ra.py
@@ -27,6 +27,7 @@ def sum_nested_structures_and_negate(structures):
             # Accumulation des sommes des tenseurs pour chaque clé
             result[key] += structure[key]            
     # Négation des résultats accumulés
+    print("ok")
     for key in result:
         result[key] = (result[key] * int(1 / (len(structures) - 2))).long()  
     return result
