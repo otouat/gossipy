25e3ce59025b8aa5f6363f1ec4696c9f83b9770a
Jezbrunon <jezekael.brunon@gmail.com>
2024-06-04 13:37:32 +0200

    debug

--- gossipy/model/architecture.py
+++ gossipy/model/architecture.py
@@ -122,52 +122,42 @@ def resnet9(num_classes):
 class ResNet50(TorchModel):
     def __init__(self, num_classes=10):
         super(ResNet50, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3)
+        self.in_planes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(64)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-
-        self.layer1 = self._make_layer(64, 64, num_blocks=3)
-        self.layer2 = self._make_layer(64, 128, num_blocks=4)
-        self.layer3 = self._make_layer(128, 256, num_blocks=6)
-
+        self.layer1 = self.make_layer(64, 64, 3)
+        self.layer2 = self.make_layer(64, 128, 4, stride=2)
+        self.layer3 = self.make_layer(128, 256, 6, stride=2)
+        self.layer4 = self.make_layer(256, 512, 3, stride=2)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(256*4, 1024)
+        self.fc = nn.Linear(512, num_classes)
+        self.init_weights()
 
-    def _make_layer(self, in_channels, out_channels, num_blocks):
+    def make_layer(self, in_channels, out_channels, num_blocks, stride=1):
         layers = []
-        layers.append(nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1))
+        layers.append(nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1))
         layers.append(nn.BatchNorm2d(out_channels))
         layers.append(nn.ReLU(inplace=True))
-
-        for _ in range(num_blocks):
-            layers.append(nn.Sequential(
-                nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
-                nn.BatchNorm2d(out_channels),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
-                nn.BatchNorm2d(out_channels)
-            ))
+        for _ in range(1, num_blocks):
+            layers.append(nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1))
+            layers.append(nn.BatchNorm2d(out_channels))
             layers.append(nn.ReLU(inplace=True))
-
         return nn.Sequential(*layers)
 
     def forward(self, x):
         x = self.conv1(x)
-        x = F.relu(F.max_pool2d(x, kernel_size=3, stride=2))
-        
+        x = self.bn1(x)
+        x = F.relu(x, inplace=True)
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
-
-        x = F.avg_pool2d(x, kernel_size=7)
-        
-        x = x.view(x.size(0), -1)  # Flatten the feature map
-        
+        x = self.layer4(x)
+        x = self.avgpool(x)  # Use the avgpool layer here
+        x = x.view(x.size(0), -1)
         x = self.fc(x)
-
         return x
 
-    def init_weights(self):  
+    def init_weights(self):  # Rename the method
         def _init(m: nn.Module):
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
@@ -178,6 +168,9 @@ class ResNet50(TorchModel):
                 nn.init.constant_(m.bias, 0)
 
         self.apply(_init)
+    
+    def __repr__(self) -> str:
+        return "Resnet50"
 
 def resnet50(num_classes):
     return ResNet50(num_classes=num_classes)
