e2b29cea2abecbece7406f898279f9e41fdd5689
Jezbrunon <jezekael.brunon@gmail.com>
2024-06-05 14:59:19 +0200

    testing

--- gossipy/model/architecture.py
+++ gossipy/model/architecture.py
@@ -175,6 +175,61 @@ class ResNet50(TorchModel):
 def resnet50(num_classes):
     return ResNet50(num_classes=num_classes)
 
+class ResNet34(TorchModel):
+    def __init__(self, num_classes=10):
+        super(ResNet34, self).__init__()
+        self.in_planes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self.make_layer(64, 64, 3)
+        self.layer2 = self.make_layer(64, 64, 4, stride=2)
+        self.layer3 = self.make_layer(64, 128, 6, stride=2)
+        self.layer4 = self.make_layer(128, 256, 3, stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(256, num_classes)
+        self.init_weights()
+
+    def make_layer(self, in_channels, out_channels, num_blocks, stride=1):
+        layers = []
+        layers.append(nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1))
+        layers.append(nn.BatchNorm2d(out_channels))
+        layers.append(nn.ReLU(inplace=True))
+        for _ in range(1, num_blocks):
+            layers.append(nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1))
+            layers.append(nn.BatchNorm2d(out_channels))
+            layers.append(nn.ReLU(inplace=True))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+    def init_weights(self):
+        def _init(m: nn.Module):
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        self.apply(_init)
+
+    def __repr__(self) -> str:
+        return "Resnet34"
+
+def resnet34(num_classes):
+    return ResNet34(num_classes=num_classes)
+
 class CIFAR10Net(TorchModel):
     def __init__(self):
         super().__init__()
--- main_jezekael_static_mia.py
+++ main_jezekael_static_mia.py
@@ -9,7 +9,7 @@ from gossipy.node import GossipNode, FederatedGossipNode, AttackGossipNode
 from gossipy.simul import AttackGossipSimulator, AttackSimulationReport
 from gossipy.model.architecture import *
 from gossipy.model.resnet import *
-from gossipy.data import *
+from gossipy.data import get_CIFAR10, get_CIFAR100
 from gossipy.topology import create_torus_topology, create_federated_topology, CustomP2PNetwork
 from gossipy.attacks.utils import log_results
 import networkx as nx
@@ -21,7 +21,7 @@ transform = Compose([Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
 train_set, test_set = get_CIFAR10()
 
 n_classes = max(train_set[1].max().item(), test_set[1].max().item())+1
-model = resnet20(n_classes)
+model = resnet34(n_classes)
 # Parameters:
 n_nodes = 100
 n_rounds = 250
