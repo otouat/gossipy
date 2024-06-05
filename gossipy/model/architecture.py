1497da3d4a96f5a34bdbb320d65897c499d7fe10
Jezbrunon <jezekael.brunon@gmail.com>
2024-06-05 16:08:12 +0200

    testing

--- gossipy/model/architecture.py
+++ gossipy/model/architecture.py
@@ -175,110 +175,68 @@ class ResNet50(TorchModel):
 def resnet50(num_classes):
     return ResNet50(num_classes=num_classes)
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-class ResNet34(nn.Module):
+class ResNet34NoRes(TorchModel):
     def __init__(self, num_classes=10):
-        super(ResNet34, self).__init__()
+        super(ResNet34NoRes, self).__init__()
         self.in_planes = 64
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3)
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-
-        self.layer1 = self.make_layer(64, 64, 3)
-        self.layer2 = self.make_layer(64, 128, 4, stride=2)
-        self.layer3 = self.make_layer(128, 256, 6, stride=2)
-        self.layer4 = self.make_layer(256, 512, 3, stride=2)
-
+        
+        self.layer1 = self.make_layer(64, 64, 6)
+        self.layer2 = self.make_layer(64, 128, 8, stride=2)
+        self.layer3 = self.make_layer(128, 256, 12, stride=2)
+        self.layer4 = self.make_layer(256, 512, 6, stride=2)
+        
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512, num_classes)
-
         self.init_weights()
 
     def make_layer(self, in_channels, out_channels, num_blocks, stride=1):
         layers = []
-        layers.append(self._make_block(in_channels, out_channels, stride))
+        layers.append(nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1))
+        layers.append(nn.BatchNorm2d(out_channels))
+        layers.append(nn.ReLU(inplace=True))
         for _ in range(1, num_blocks):
-            layers.append(self._make_block(out_channels, out_channels))
+            layers.append(nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1))
+            layers.append(nn.BatchNorm2d(out_channels))
+            layers.append(nn.ReLU(inplace=True))
         return nn.Sequential(*layers)
-
-    def _make_block(self, in_channels, out_channels, stride=1):
-        downsample = None
-        if stride != 1 or in_channels != out_channels:
-            downsample = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels),
-            )
-
-        return BasicBlock(in_channels, out_channels, stride, downsample)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
         x = self.maxpool(x)
-
+        
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-
+        
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.fc(x)
-
         return x
 
     def init_weights(self):
-        def _init(m):
+        def _init(m: nn.Module):
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
             elif isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, 0, 0.01)
-                nn.init.constant_(m.bias, 0)
 
         self.apply(_init)
+    
+    def __repr__(self) -> str:
+        return "ResNet34NoRes"
 
-class BasicBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
-        super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        self.downsample = downsample
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
-        return out
-
-def resnet34(num_classes=10):
-    return ResNet34(num_classes=num_classes)
-
-# Example usage
-# model = resnet34(num_classes=10)
-# print(model)
+def resnet34_no_res(num_classes):
+    return ResNet34NoRes(num_classes=num_classes)
 
 #-------------------------------------------------- TESTING ---------------------------------------------------#
 
