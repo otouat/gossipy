1a7ea8e1a05d8551254df6c16e525a421ddc1264
Jezbrunon <jezekael.brunon@gmail.com>
2024-06-06 09:57:51 +0200

    testing

--- main_test.py
+++ main_test.py
@@ -112,6 +112,6 @@ simulator = AttackGossipSimulator(
 report = AttackSimulationReport()
 simulator.add_receiver(report)
 simulator.init_nodes(seed=42)
-simulator.start(n_rounds=n_rounds)
+simulator.start(n_rounds=n_rounds, wall_time_limit=11.5)
 
 log_results(simulator, report, message)
