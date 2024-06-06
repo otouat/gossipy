e2bc6a379c23de266529538ddad6861e8e5233c4
Jezbrunon <jezekael.brunon@gmail.com>
2024-06-06 12:27:09 +0200

    testing

--- main_jezekael_static_mia.py
+++ main_jezekael_static_mia.py
@@ -116,5 +116,5 @@ simulator.add_receiver(report)
 simulator.init_nodes(seed=42)
 simulator.start(n_rounds=wdb.epochs, wall_time_limit=0.1)
 
-log_results(simulator, report, message)
+log_results(simulator, report, wandb, message)
 wandb.finish()
