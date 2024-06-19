4ad38d9035f8beaab64aed97dfbd260a70da4cf8
Jezbrunon <jezekael.brunon@gmail.com>
2024-06-19 17:16:22 +0200

    debug

--- main_jezekael_dynamic_mia.py
+++ main_jezekael_dynamic_mia.py
@@ -35,7 +35,7 @@ config = {
     "dataset": "CIFAR-10",
     "epochs": 250,
     "batch_size": 256,
-    "n_nodes": 100,
+    "n_nodes": 36,
     "n_local_epochs": 3,
     "neigbors": 2,
     "test_size": 0.5,
