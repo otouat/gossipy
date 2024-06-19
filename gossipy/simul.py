c320a06dfae2ad48cf9b8e08b604863c2fede72b
Jezbrunon <jezekael.brunon@gmail.com>
2024-06-19 15:55:41 +0200

    d

--- gossipy/simul.py
+++ gossipy/simul.py
@@ -1439,10 +1439,12 @@ class AttackDynamicGossipSimulator(GossipSimulator):
                     if node.timed_out(t):
                         if isinstance(node.p2p_net, UniformDynamicP2PNetwork) and t % self.peer_sampling_period == 0:
                             print(node)
-                            print(node.__dir__())
+                            #print(node.__dir__())
+                            print(node.p2p_net)
+                            print(node.idx)
                             for n in node.p2p_net._topology[node]:
                                 print(n)
-                                print(n.__dir__())
+                                #print(n.__dir__())
                             node.p2p_net.update_view(node_id=i)
                         peer = node.get_peer()
                         if peer is None:
